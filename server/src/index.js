26d16b5e57957cdbca68eca0c57b25595cab7531
jralvarezwindey <jralvarezwindey@gmail.com>
2023-04-26 11:43:31 -0300

    .

--- server/src/index.js
+++ server/src/index.js
@@ -16,20 +16,20 @@
 //         \  \ `_.   \_ __\ /__ _/   .-` /  /
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-require('dotenv').config();
-const app = require('./app.js');
-const db = require('./db.js');
-const dietsLoader = require('./preloaders/dietsLoader.js')
-const recipesLoader = require('./preloaders/recipesLoader.js')
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require("dotenv").config();
+const app = require("./app.js");
+const db = require("./db.js");
+const dietsLoader = require("./preloaders/dietsLoader.js");
+const recipesLoader = require("./preloaders/recipesLoader.js");
 
 // Syncing all the models at once.
 db.sync({ force: true }).then(async () => {
-  console.log('Data base created.');
+  console.log("Data base created.");
   await Promise.all(dietsLoader());
-  console.log('Diets loaded.');
+  console.log("Diets loaded.");
   await Promise.all(recipesLoader());
-  console.log('Default recipes loaded.');
+  console.log("Default recipes loaded.");
   app.listen(process.env.PORT, () => {
     console.log(`Server listening at ${process.env.PORT}.`);
   });
