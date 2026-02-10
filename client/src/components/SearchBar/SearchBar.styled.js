044ec737ea8e48c07096e3a6c111bc49f0d5cac0
Julian Alvarez <juroalwi@gmail.com>
2026-02-09 21:31:50 -0300

    remove SearchBar unset

--- client/src/components/SearchBar/SearchBar.styled.js
+++ client/src/components/SearchBar/SearchBar.styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import { colors } from "../GlobalStyle.styled";
 
 export const Query = styled.input`
-  all: unset;
   padding: 15px 30px;
   width: 100%;
   max-width: 600px;
