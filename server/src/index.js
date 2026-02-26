069480c5d312cd99de87cc4c92d797e3405fc647
Julian Alvarez <juroalwi@gmail.com>
2026-02-26 20:23:20 -0300

    port var

--- server/src/index.js
+++ server/src/index.js
@@ -4,13 +4,15 @@ import { app } from "./app.js";
 import { dietsLoader } from "./preloaders/dietsLoader.js";
 import { recipesLoader } from "./preloaders/recipesLoader.js";
 
+const port = process.env.PORT || 3000;
+
 sequelize.sync({ force: true }).then(async () => {
   console.log("Data base created.");
   await Promise.all(dietsLoader());
   console.log("Diets loaded.");
   await Promise.all(recipesLoader());
   console.log("Default recipes loaded.");
-  app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server listening at ${process.env.PORT || 3000}.`);
+  app.listen(port, () => {
+    console.log(`Server listening at ${port}.`);
   });
 });
