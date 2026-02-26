75df868f8f180b0c794907533d6188458dc5b295
Julian Alvarez <juroalwi@gmail.com>
2026-02-26 20:20:54 -0300

    separate getRecipeDetail db from api logic

--- server/src/routes/recipes/getRecipeDetail.js
+++ server/src/routes/recipes/getRecipeDetail.js
@@ -3,51 +3,88 @@ import { recipeModel, dietModel } from "../../db.js";
 
 export const getRecipeDetail = async (req, res, next) => {
   try {
-    const id = parseInt(req.params.id.replace("EXTERNAL_", ""), 10);
+    const id = req.params.id;
 
-    if (req.params.id.includes("EXTERNAL_")) {
-      const response = await axios.get(
-        `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`,
-      );
-      const recipe = response.data;
-      const { title, image, diets, summary, spoonacularScore, healthScore } =
-        recipe;
-      const instructions =
-        recipe.analyzedInstructions.length > 0
-          ? recipe.analyzedInstructions[0].steps.map((step) => step.step)
-          : [];
-
-      res.status(200).send({
-        id: "EXTERNAL_" + recipe.id,
-        title,
-        image,
-        diets,
-        summary: summary.replace(/<[^>]*>?/g, ""),
-        score: spoonacularScore / 20,
-        healthScore,
-        instructions,
-      });
-    } else {
-      const response = await Recipe.findByPk(id, {
-        include: {
-          model: Diet,
-          as: "diets",
-          attributes: ["name"],
-          through: { attributes: [] },
-        },
-      });
-
-      const recipe =
-        response !== null
-          ? {
-              ...response?.dataValues,
-              diets: response.dataValues.diets.map((diet) => diet.name),
-            }
-          : {};
-
-      res.send(recipe);
+    if (!id) {
+      res.status(400).send("Invalid recipe ID provided.");
     }
+
+    const internalRecipe = await getInternalRecipe(id);
+
+    if (internalRecipe) {
+      return res.status(200).send(internalResult);
+    }
+
+    if (!internalRecipe && id.includes("CREATED_")) {
+      return res.status(404).send("Recipe not found");
+    }
+
+    const externalRecipe = await getExternalRecipe(id);
+
+    if (!externalRecipe) {
+      return res.status(404).send("Recipe not found");
+    }
+
+    return res.status(200).send(externalRecipe);
   } catch (error) {
+    console.error(`Failed to get recipe detail ${id}: ${error}`);
     next(error);
+  }
+};
+
+const getInternalRecipe = async (id) => {
+  try {
+    const response = await recipeModel.findByPk(id, {
+      include: {
+        model: dietModel,
+        as: "diets",
+        attributes: ["name"],
+        through: { attributes: [] },
+      },
+    });
+
+    if (!response || !response.dataValues) {
+      return null;
+    }
+
+    return {
+      ...response.dataValues,
+      diets: response.dataValues.diets.map((diet) => diet.name),
+    };
+  } catch (error) {
+    console.error(`Failed to get internal recipe detail ${id}: ${error}`);
+    return null;
+  }
+};
+
+const getExternalRecipe = async (id) => {
+  try {
+    const response = await axios.get(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.API_KEY}`,
+    );
+
+    const recipe = response.data;
+
+    const { id, title, image, diets, summary, spoonacularScore, healthScore } =
+      recipe;
+
+    const instructions =
+      recipe.analyzedInstructions.length > 0
+        ? recipe.analyzedInstructions[0].steps.map((step) => step.step)
+        : [];
+
+    return {
+      id,
+      title,
+      image,
+      diets,
+      summary: summary.replace(/<[^>]*>?/g, ""),
+      score: spoonacularScore,
+      healthScore,
+      instructions,
+    };
+  } catch (error) {
+    console.error(`Failed to get internal recipe detail ${id}: ${error}`);
+    return null;
   }
 };
