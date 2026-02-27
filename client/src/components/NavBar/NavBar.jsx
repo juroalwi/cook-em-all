47b4708f7a806b18c701ec9b1d86dae0ea90f228
Julian Alvarez <juroalwi@gmail.com>
2026-02-27 13:21:14 -0300

    better navbar styles

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar.jsx";
-import logo from "../../media/logo.svg";
 import ScreenSizeProvider from "../../providers/ScreenSizeProvider.jsx";
 
 export default function NavBar() {
@@ -12,16 +11,17 @@ export default function NavBar() {
 
   return (
     <ScreenSizeProvider>
-      <nav className="lg:bg-custom-black flex flex-col-reverse items-center justify-between gap-4 bg-transparent px-4 py-2 lg:flex-row lg:gap-16 lg:px-8 lg:py-4">
+      <nav className="lg:light-shadow-small flex flex-col-reverse items-center justify-between gap-4 bg-transparent px-4 py-2 lg:flex-row lg:gap-16 lg:px-8 lg:py-4">
         <div className="hidden w-full items-center justify-center gap-4 lg:flex">
-          <img
-            src={logo}
-            onClick={handleLogoClick}
-            className="h-17.5 cursor-pointer invert-100"
-            alt="Cook 'Em All Logo"
-          />
-          <div className="text-custom-white text-2xl tracking-wider uppercase">
-            Cook 'Em All
+          <div className="flex items-center gap-4 cursor-pointer" onClick={handleLogoClick}>
+            <img
+              src="/logo.svg"
+              className="h-17.5 invert-100"
+              alt="Cook 'Em All Logo"
+            />
+            <div className="text-custom-white text-2xl tracking-wider uppercase">
+              Cook 'Em All
+            </div>
           </div>
         </div>
 
