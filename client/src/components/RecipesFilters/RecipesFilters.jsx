e1f771d32e1d49fa4e56b4b223c8b536c143516f
Julian Alvarez <juroalwi@gmail.com>
2026-03-10 10:30:26 -0300

    recipes filters rounded borders

--- client/src/components/RecipesFilters/RecipesFilters.jsx
+++ client/src/components/RecipesFilters/RecipesFilters.jsx
@@ -1,7 +1,3 @@
-import useRecipes from "../../hooks/useRecipes.js";
-import useDiets from "../../hooks/useDiets.js";
-import useScreenSize from "../../hooks/useScreenSize.js";
-import FiltersIcon from "../../media/icons/FiltersIcon.jsx";
 import { useState } from "react";
 import { useRecipes } from "../../hooks/useRecipes.js";
 import { useDiets } from "../../hooks/useDiets.js";
@@ -46,14 +42,14 @@ export const RecipesFilters = () => {
   return (
     <div className="text-custom-white flex items-center gap-16">
       <div className="group relative cursor-pointer">
-        <div className="light-shadow-small w-60 px-12 py-3 text-center text-xl font-medium transition-all duration-200 group-hover:opacity-80 group-hover:shadow-none rounded-xs">
+        <div className="light-shadow-small w-60 rounded-xs px-12 py-3 text-center text-xl font-medium transition-all duration-200 group-hover:opacity-80 group-hover:shadow-none">
           Sort by
           {recipesSortBy
             ? `: ${recipesSortBy === "az" ? "a-z" : recipesSortBy === "za" ? "z-a" : "score"}`
             : ""}
         </div>
 
-        <div className="absolute z-10 hidden w-full bg-black shadow-[2px_4px_16px_0px_rgba(45,45,45,1)] group-hover:block rounded-xs">
+        <div className="absolute z-10 hidden w-full rounded-xs bg-black shadow-[2px_4px_16px_0px_rgba(45,45,45,1)] group-hover:block">
           <p
             className={`hover:bg-custom-white hover:text-custom-black px-4 py-3 text-xl tracking-wide transition-colors duration-200 ${
               recipesSortBy === "az" ? "text-custom-red" : "text-custom-white"
@@ -87,7 +83,7 @@ export const RecipesFilters = () => {
         <div className="w-60 px-12 py-3 text-center text-xl font-medium shadow-[1px_2px_8px_0px_rgba(45,45,45,1)] transition-all duration-200 group-hover:opacity-80 group-hover:shadow-none">
           Filters ({recipesFilters.length})
         </div>
-        <div className="absolute z-10 hidden w-100 flex-wrap gap-2.5 bg-black p-4 shadow-[2px_4px_16px_0px_rgba(45,45,45,1)] group-hover:flex rounded-xs">
+        <div className="absolute z-10 hidden w-100 flex-wrap gap-2.5 rounded-xs bg-black p-4 shadow-[2px_4px_16px_0px_rgba(45,45,45,1)] group-hover:flex">
           {diets.map((diet, index) => {
             const active = recipesFilters.includes(diet);
             return (
@@ -126,7 +122,7 @@ const RecipesFiltersMobile = ({
       </button>
 
       {isOpen && (
-        <div className="light-shadow absolute top-full z-10 flex w-[90%] flex-col gap-2 bg-black p-4 rounded-xs">
+        <div className="light-shadow absolute top-full z-10 flex w-[90%] flex-col gap-2 rounded-xs bg-black p-4">
           <div className="text-lg font-semibold">Sort by</div>
 
           <div className="flex cursor-pointer gap-4 p-2">
@@ -179,4 +175,4 @@ const RecipesFiltersMobile = ({
       )}
     </div>
   );
-}
+};
